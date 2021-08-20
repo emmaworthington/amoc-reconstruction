3f2b6d57229bfe5dacb41ba6c9c84651cb055a5c
Emma Worthington <elw2u16@soton.ac.uk>
2021-08-20 15:47:32 +0100

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 30 18:01:04 2021
+
+@author: emmaworthington
+
+Run the code required to produce the main results for the paper:
+    
+"A 30-year reconstruction of the Atlantic meridional 
+overturning circulation shows no decline."
+
+https://doi.org/10.5194/os-17-1-2021
+
+"""
+
+# 
+
